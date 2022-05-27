ab02e211f71362ecb9b8b7c74ebd9c58c4422616
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-05-27 22:48:44 +0500

    Add logging to base cmd classes

--- commands/base.py
+++ commands/base.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class CmdExecutor:
     """Base chat commands container, which implement execute method for it"""
 
@@ -7,6 +10,7 @@ class CmdExecutor:
 
     async def execute(self, message) -> str:
         """reads command from message and executes helper method for it"""
+        logging.debug("CmdExecutor 'execute' method called with message: %s", message.content)
         self.msg = message
         split = message.content.split()
 
@@ -14,9 +18,12 @@ class CmdExecutor:
             cmd = split[0]
             params = split[1:]
         except IndexError:
+            logging.debug("CmdExecutor 'execute' method raise IndexError exception")
             return ''
 
+        logging.debug(f"CmdExecutor split message to cmd '{cmd}' and params '{params}'")
         if cmd in self._commands:
+            logging.info(f"CmdExecutor find command '{cmd}' and call handled method with params '{params}'")
             return await self._commands[cmd](params)
         return ''
 
@@ -28,16 +35,29 @@ class CmdParser:
         self._executors = []
 
     def add(self, cmd_executor) -> None:
-        self._executors.append(cmd_executor)
+        logging.debug("CmdParser 'add' method called with param: %s", cmd_executor)
+
+        if cmd_executor not in self._executors:
+            self._executors.append(cmd_executor)
+            logging.info("New executor '%s' added in parser. Parser now contain: %s", cmd_executor, self._executors)
 
     def delete(self, cmd_executor) -> None:
+        logging.debug("CmdParser 'delete' method called with param: %s", cmd_executor)
+
         if cmd_executor in self._executors:
             self._executors.remove(cmd_executor)
+            logging.info("Executor '%s' deleted from parser. Parser now contain: %s", cmd_executor, self._executors)
 
     async def parse(self, message) -> str:
-        """executes all parsers to process cmd"""
+        """Executes all parsers to process cmd
+
+        :return: some data string if success else empty string
+        """
+        logging.debug("CmdParser 'parse' method called with message: %s", message)
+
         for e in self._executors:
             result = await e.execute(message)
             if result:
+                logging.info("Parser 'parse' method successful with result: %s", result)
                 return result
         return ''
