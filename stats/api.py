c912065155dd9c71181dded5bf7f5db2876bd614
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-05-30 00:37:33 +0500

    add logging

--- stats/api.py
+++ stats/api.py
@@ -1,9 +1,15 @@
+import logging
 import aiohttp
 
 
 async def get_response(session: aiohttp.ClientSession, url: str, params: dict) -> tuple:
+    logging.debug(f"get_response called with {url} and {params}")
+
     async with session.get(url, params=params) as response:
-        return response.status, await response.text()
+        status = response.status
+        text = await response.text()
+        logging.debug(f"get_response exit with status:{status} and data:{text}")
+        return status, text
 
 
 class StatsApi:
@@ -16,6 +22,8 @@ class StatsApi:
             'start': '0',
             'count': '1'
         }
+        logging.debug(f"StatsApi 'rating' method called with url:{url} and params "
+                      f"steam:{steam_id} profile:{profile_id} leaderboard:{leaderboard_id}")
 
         if steam_id and steam_id != '':
             params['steam_id'] = steam_id
