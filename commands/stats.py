f56cc7f7d5ce6705d3d34dc8ff96e3c0bba69829
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-05-31 16:58:43 +0500

    create handlers for rank and match commands

--- commands/stats.py
+++ commands/stats.py
@@ -1,14 +1,25 @@
+import aiohttp
 import logging
 from commands.base import CmdExecutor
+from stats.checker import StatsChecker
 
 
 class StatsExecutor(CmdExecutor):
     def __init__(self):
         super().__init__()
         self._commands = {
-            '/rank': self.rank
+            '/rank2': self.rank,
+            '/match': self.match
         }
 
     async def rank(self, param):
         logging.debug(f"StatsExecutor.rank: CALLED")
-        return 'rank'
+        async with aiohttp.ClientSession() as s:
+            steam_id = param[0]
+            return await StatsChecker.rank(s, steam_id)
+
+    async def match(self, param):
+        logging.debug(f"StatsExecutor.match: CALLED")
+        async with aiohttp.ClientSession() as s:
+            steam_id = param[0]
+            return await StatsChecker.match(s, steam_id)
