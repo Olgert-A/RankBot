66ce3cbdc53e116ba0da920d854044533f2cf141
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-05-31 10:43:39 +0500

    Update parser.py
    Data from request now unpacked and send to convert_rank method to convert it to text format

--- stats/parser.py
+++ stats/parser.py
@@ -1,5 +1,3 @@
-import logging
-
 import aiohttp
 import asyncio
 from stats.api import StatsApi as Api
@@ -12,9 +10,9 @@ class StatsParser:
         leaderboards = [lb for lb in LeaderboardID if lb != LeaderboardID.UNRANKED]
         tasks = [Api.rating(session, steam_id, profile_id, lb.value) for lb in leaderboards]
         ranks_raw = await asyncio.gather(*[asyncio.create_task(t) for t in tasks])
-        logging.info(f"StatsParser 'rank' method return data: {ranks_raw}")
-        ranks1 = [convert_rank(status, data) for status, data in ranks_raw]
-        return ""
+        ranks = [convert_rank(lb.name, status, data) for lb, (status, data) in zip(leaderboards, ranks_raw)]
+        logging.info(f"StatsParser 'rank' success with data: {ranks}")
+        return ' | '.join(ranks)
 
     @staticmethod
     async def match(session: aiohttp.ClientSession, steam_id: str) -> str:
