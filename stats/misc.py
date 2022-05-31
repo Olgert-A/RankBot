72a457c732579cf642725066e318c9b4eb85e556
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-05-31 13:23:15 +0500

    debug msg + add winrate method
    1) change debug messages
    2) move winrate calculation to separate method

--- stats/misc.py
+++ stats/misc.py
@@ -3,23 +3,34 @@ import json
 import logging
 
 
-def formatted(resp: str) -> str:
-    """remove brackets [] in rating response"""
+def remove_brackets(resp: str) -> str:
+    """remove start and end brackets ->[data]<- in rating response"""
     return resp[1:len(resp) - 1]
 
 
+def calc_winrate(wins: int, losses: int) -> int:
+    """calculate winrate using number of wins and number of losses"""
+    if not (wins + losses):
+        return round(100 * wins / (wins + losses))
+    else:
+        return 0
+
+
 def convert_rank(leaderboard: str, status: int, data: str) -> str:
+    """parse rank query results into string"""
+    logging.debug(f'convert_rank: CALLED     leaderboard: {leaderboard} status: {status} data:{data}')
     if status == 200:
-        data = formatted(data)
+        data = remove_brackets(data)
         try:
             data = json.loads(data)
-            wins = int(data['num_wins'])
-            loses = data['num_losses']
-            winrate = round(100 * wins / (wins + loses))
-            return f"{leaderboard}:{data['rating']} {winrate}% {data['streak']}"
+            logging.debug(f'convert_rank: unpacked data to json: {data}')
+            winrate = calc_winrate(data['num_wins'], data['num_losses'])
+            result = f"{leaderboard}:{data['rating']} {winrate}% {data['streak']}"
+            logging.debug(f'convert_rank: SUCCESS     result: {result}')
+            return result
 
         except ValueError:
-            logging.debug('Rank conversion raise exception')
+            logging.exception('convert_rank: conversion raise exception')
             return '----'
     else:
         return '----'
@@ -27,5 +38,5 @@ def convert_rank(leaderboard: str, status: int, data: str) -> str:
 
 class LeaderboardID(enum.Enum):
     S = 3  # SOLO
-    TG = 4  # TG
+    TG = 4  # TeamGames
     UNRANKED = 0
