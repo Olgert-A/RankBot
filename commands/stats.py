160389aae9ece1d0d9dc8c9f64a27a43703bd892
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-01 13:22:08 +0500

    execute stats checker

--- commands/stats.py
+++ commands/stats.py
@@ -1,12 +1,19 @@
+import aiohttp
+import logging
 from commands.base import CmdExecutor
+from stats.checker import StatsChecker
 
 
 class StatsExecutor(CmdExecutor):
     def __init__(self):
         super().__init__()
         self._commands = {
-            '/rank': self.rank
+            '/rank': self.rank,
+            '/rank2': self.rank
         }
 
     async def rank(self, params):
-        return 'rank'
+        logging.debug(f"StatsExecutor handled '/rank' cmd with parameters: {params}")
+        async with aiohttp.ClientSession() as s:
+            steam_id = params[0]
+            return await StatsChecker.rank(s, steam_id)
